9c38aa242f737f61bcb9aac897a39402fae8903c
Gavin Quirk <gavinquirk@gmail.com>
2019-02-05 17:16:25 -0800

    added uploader and togglemounting components from docs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+// import ToggleMounting from "./components/ToggleMounting";
+import Uploader from "./components/Uploader";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>TESTING</p>
+
+        {/* <ToggleMounting> */}
+          <label>Image Upload</label>
+          <Uploader
+            id="image"
+            name="image"
+            data-images-only />
+
+        {/* </ToggleMounting> */}
+        
       </div>
     );
   }
